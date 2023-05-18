65751125a12598046da04858454e432e5d3e2fbb
mikolaj <mikolaj.pietruczuk03@gmail.com>
2023-05-18 15:53:10 +0200

    Weapons picking and dropping working (E/Q)

--- offline.py
+++ offline.py
@@ -2,6 +2,7 @@ import pygame
 import os
 import sys
 import ctypes
+import time
 
 
 pygame.init()
@@ -15,8 +16,12 @@ GAME_WINDOW = pygame.display.set_mode((WIDTH - 20, HEIGHT - 20), pygame.RESIZABL
 BG = (135,206,235)
 ANIMATION_SPEED = 10
 PLAYER_SIZE= [100, 100]
-BARREL_SIZE = [100, 100]
+BARREL_SIZE = (177/3, 238/3)
+WEAPON_SIZE = (412/7,166/7)
+BULLET_SIZE = (32/3, 80/3)
 
+
+##To Maximize the Window Size ONLY FOR WINDOWS
 if sys.platform == "win32":
     HWND = pygame.display.get_wm_info()['window']
     SW_MAXIMIZE = 3
@@ -26,13 +31,35 @@ class Object():
     def __init__(self):
         self.image = pygame.image.load('sprites\sci-fiPlatform\png\Objects\Barrel (1).png')
         self.image = pygame.transform.scale(self.image, BARREL_SIZE)
-        self.rect = self.image.get_rect()
+        self.rect = pygame.Rect(500, 500, 177/3, 238/3)
         self.rect.x = 500
         self.rect.y = 500
 
     def updateObject(self):
-        pygame.draw.rect(GAME_WINDOW, (255 , 255, 255), self.rect) 
         GAME_WINDOW.blit(self.image, self.rect)
+
+
+class Weapon():
+    def __init__(self):
+        self.image = pygame.image.load('sprites/weapons/pistol3.png')
+        self.image = pygame.transform.scale(self.image, WEAPON_SIZE)
+        self.rect = pygame.Rect(700, 700, 412/7, 166/7)
+        self.rect.x = 700
+        self.rect.y = 700
+    
+    
+    def updateWeapon(self):
+        GAME_WINDOW.blit(self.image, self.rect)
+
+
+class Bullet():
+    def __init__(self):
+        self.image = pygame.transform.scale(pygame.image.load('sprites/weapons/small_bullet2.png'), BULLET_SIZE)
+        self.rect = pygame.Rect
+        
+
+
+
 
 class Player():
     def __init__(self):
@@ -43,14 +70,15 @@ class Player():
         self.imageIndex = 0
         self.animationCooldown = 0
         self.position = ""
+        self.weapon = False
+        self.ammo = []
+        self.health = []
 
         for i in range(1, 4):
             image = pygame.image.load(f'sprites\player{i}.png')
             image = pygame.transform.scale(image, PLAYER_SIZE)
             if i == 1:
-                self.rect = image.get_rect()
-                self.rect.x = 300
-                self.rect.y = 200
+                self.rect = pygame.Rect(200, 300, PLAYER_SIZE[0], PLAYER_SIZE[1])
             self.imagesAnimationUp.append(image)
 
         self.imagesAnimationDown = []
@@ -73,13 +101,13 @@ class Player():
         self.image = self.imagesAnimationUp[self.imageIndex]
     
 
-    def updatePlayer(self):
+    def updatePlayer(self, keys):
         PLAYER_SPEED_X = 0
-        PLAYER_SPEED_Y = 0
-        pygame.draw.rect(GAME_WINDOW, (255 , 255, 255), self.rect) 
+        PLAYER_SPEED_Y = 0 
         GAME_WINDOW.blit(self.image, self.rect)   
 
-        keys = pygame.key.get_pressed()
+
+        #handling Movement
         if keys[pygame.K_w] and self.rect.y > 0:
             PLAYER_SPEED_Y = -15
             self.animationCooldown += 1
@@ -124,6 +152,7 @@ class Player():
                 self.animationCooldown = 0
             self.position = "LEFT"
 
+        #colliding with objects
         match self.position:
             case "UP":
                 if testObject1.rect.colliderect(self.rect.x, self.rect.y + PLAYER_SPEED_Y, self.rect.width, self.rect.height):
@@ -138,6 +167,7 @@ class Player():
                 if testObject1.rect.colliderect(self.rect.x + PLAYER_SPEED_X, self.rect.y, self.rect.width, self.rect.height):
                     PLAYER_SPEED_X = 0
 
+        #Handling animations when stopped
         if keys[pygame.K_w] == False and keys[pygame.K_s] == False and keys[pygame.K_a] == False and keys[pygame.K_d] == False:
             match self.position:
                 case "UP":
@@ -152,6 +182,8 @@ class Player():
                 case "RIGHT":
                     self.imageIndex = 0
                     self.image = self.imagesAnimationRight[self.imageIndex]
+
+        #Movement of the Player Rectangle
         match self.position:
             case "UP":
                 self.rect.y += PLAYER_SPEED_Y
@@ -164,11 +196,36 @@ class Player():
 
 
 
+    
 
-def drawWindow():
+    def checkForWeaponDetection(self, events):
+        for event in events:
+            if testWeapon1.rect.colliderect(self.rect.x + 20, self.rect.y + 20, self.rect.height + 20, self.rect.width + 20) and event.type == pygame.KEYDOWN and self.weapon != True:
+                if(event.key == pygame.K_e):
+                    self.weapon = True
+            
+            if self.weapon == True and event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_q:
+                    self.weapon = False
+                
+                
+        if self.weapon:
+            testWeapon1.rect.x = self.rect.x + 65
+            testWeapon1.rect.y = self.rect.y + 55
+
+        
+
+        
+
+
+
+
+
+def drawWindow(keys):
     GAME_WINDOW.fill(BG)
-    player1.updatePlayer()
+    player1.updatePlayer(keys)
     testObject1.updateObject()
+    testWeapon1.updateWeapon()
 
 def drawCrosshair():
     x, y = pygame.mouse.get_pos()
@@ -180,6 +237,7 @@ def drawCrosshair():
 
 player1 = Player()
 testObject1 = Object()
+testWeapon1 = Weapon()
 
 def main():
     clock = pygame.time.Clock()
@@ -188,10 +246,16 @@ def main():
     pygame.mouse.set_visible(False)
     while run:
         clock.tick(FPS)
-        for event in pygame.event.get():
+        events = pygame.event.get()    
+        userInput = pygame.key.get_pressed()
+
+        for event in events:
             if event.type == pygame.QUIT:
                 run = False
-        drawWindow()
+    
+
+        player1.checkForWeaponDetection(events)
+        drawWindow(userInput)
         drawCrosshair()
         pygame.display.update()
 
